d34fe60b22578418dbae5cbf2f4ce72aff3ca28b
Javohir <gofurovjavohir55@gmail.com>
2024-03-25 11:35:28 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+alert(`Assalomu Alaykum`)
+confirm(`Bizning saytimizga xush kelibsiz`)
+prompt(`Ismingizni kiriting`)
+
+
+
+const openEye = document.querySelector(".bi-eye");
+const closeEye = document.querySelector(".bi-eye-slash");
+const passwordEl = document.querySelector(".password");
+
+closeEye.addEventListener("click", () => {
+  openEye.style.display = "block";
+  closeEye.style.display = "none";
+  passwordEl.setAttribute("type", "text");
+});
+
+openEye.addEventListener("click", () => {
+  closeEye.style.display = "block";
+  openEye.style.display = "none";
+  passwordEl.setAttribute("type", "password");
+});
